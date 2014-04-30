fba13abb13207157ff796501d68ffdd2b6022850
Arjen Cleij <arjen@wayneparkerkent.com>
2014-04-30 18:02:58 +0200

    Google Maps

--- map_poi.php
+++ map_poi.php
@@ -1,7 +1,7 @@
 <div class='mapp-iw'>
 	<div class='mapp-body'>
-		<?php echo $poi->get_title_link(); ?>
-		<?php echo $poi->get_thumbnail(array('class' => 'mapp-thumb')); ?>
+		<?php //echo $poi->get_title_link(); ?>
+		<?php //echo $poi->get_thumbnail(array('class' => 'mapp-thumb')); ?>
 		<?php echo $poi->get_body(); ?>
 	</div>
 </div>
