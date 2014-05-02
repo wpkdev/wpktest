1b1a839e17b890aaa2911b45b8290d05eadca08b
Arjen Cleij <arjen@wayneparkerkent.com>
2014-05-02 16:20:16 +0200

    Redirect page fix
    duh

--- page-out.php
+++ page-out.php
@@ -9,21 +9,20 @@ $shop_clean_url = explode( '/', $shop_url );
 
 ?>
 
-<div class="shop-redirect">
-
-	<div class="content">
-		<h1>Je verlaat nu shotofjoy.nl</h1>
-		<p>We sturen je door naar <a href="<?php echo $shop_url;?>"><?php echo $shop_clean_url[2]; ?></a></p>
+	<div class="shop-redirect">
+	
+		<div class="content">
+			<h1>Je verlaat nu shotofjoy.nl</h1>
+			<p>We sturen je door naar <a href="<?php echo $shop_url;?>"><?php echo $shop_clean_url[2]; ?></a></p>
+		</div>
+	
 	</div>
-
-</div>
-<script>
-$('body').addClass('page-redirect');
-$('#wrapper').addClass('center');
-
-//setTimeout("top.location.href = '<?php echo $redirect_url;?>/'",3000);
-
-</script>
+	<script>
+		$('body').addClass('page-redirect');
+		$('#wrapper').addClass('center');
+		
+		setTimeout("top.location.href = '<?php echo $shop_url;?>/'",3000);
+	</script>
 
 </div><!-- wrapper -->
 </body>
