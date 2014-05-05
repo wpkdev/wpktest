febc9553c0cb0f9c9aa492ca1f76115193d51796
Arjen Cleij <arjen@wayneparkerkent.com>
2014-05-05 16:02:56 +0200

    Welcome popup

--- incl_homepopup.php
+++ incl_homepopup.php
@@ -1,8 +1,9 @@
 <div class="welcomepopup-box">
-	<div class="welcomepopup center">
+	<a href="#" class="btn btn-circle btn-close">X</a>
+	<div class="welcomepopup center">	
 		<img src="<?php echo $template_directory; ?>/img/SOJ-logo.png" alt="Shot of Joy">
 		<h1>het allerbeste begin van de dag</h1>
 		<p>Dagelijks voor maar &euro; 2,99 per maand</p>
-		<p><a href="#" class="btn btn-soj btn-small">Login</a> <a href="#" class="btn btn-soj btn-small">Abonneer</a></p>
+		<p><a href="<?php echo get_site_url(); ?>/login" class="btn btn-soj btn-small">Login</a> <a href="<?php echo get_site_url();?>/register" class="btn btn-soj btn-small">Abonneer</a></p>
 	</div>
 </div>
