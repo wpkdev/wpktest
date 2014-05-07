bb9d3e92bf892485515f56c9d48e7e24eebc3e0f
Arjen Cleij <arjen@wayneparkerkent.com>
2014-05-07 17:26:30 +0200

    Page Quote font-size

--- incl_large_image.php
+++ incl_large_image.php
@@ -10,16 +10,13 @@
 	$quote = get_sub_field('quote');
 	$color_scheme = get_sub_field('colorscheme');
 	
-	echo '<div class="page-element page-'.$counter.' " data-src="'.$src[0].'" data-src-1024="'.$src_1024[0].'" data-src-768="'.$src_768[0].'" data-src-480="'.$src_480[0].'">';
+	echo '<div class="page-element page-'.$counter.'" data-src="'.$src[0].'" data-src-1024="'.$src_1024[0].'" data-src-768="'.$src_768[0].'" data-src-480="'.$src_480[0].'">';
 	//echo '<img src="'.get_bloginfo('template_directory').'/img/black.gif" class=" page-bg-'.$counter.' bg stickem"   />';
 		$position_horizontal = get_sub_field('title_image_horizontal');
 		$position_vertical = get_sub_field('title_image_vertical');
 		$image_title = get_sub_field('title_image');
-		echo '<div class="page-content page-content-box start-page  vert-'.$position_vertical.'-title hor-'.$position_horizontal.'-title  page-content-'.$color_scheme.' "><h1 class="main-title">';
+		echo '<div class="page-content page-content-box page-quote start-page vert-'.$position_vertical.'-title hor-'.$position_horizontal.'-title  page-content-'.$color_scheme.' "><h1 class="main-title">';
 			echo $quote;
-			
-			
-			
 		echo '</h1>';
 		
 		$sub_title = get_sub_field('subtitle');
@@ -28,8 +25,6 @@
 		}
 		echo '</div>';
 		
-		echo '<div class="content-large-image">';
+		echo '<div class="content-large-image"><img src="'.$src[0].'" class=" page-bg-'.$counter.' bg" /></div>';
 		
-		echo '<img src="'.$src[0].'" class=" page-bg-'.$counter.' bg"   />';
-		
-		echo '</div></div>';
+		echo '</div>';
