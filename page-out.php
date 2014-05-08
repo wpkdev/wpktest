fc969c34d9391b58d9c35f2bda24a75b7d4d3484
Arjen Cleij <arjen@wayneparkerkent.com>
2014-05-08 15:17:19 +0200

    Logo on redirect page

--- page-out.php
+++ page-out.php
@@ -10,7 +10,7 @@ $shop_clean_url = explode( '/', $shop_url );
 ?>
 
 	<div class="shop-redirect">
-	
+		<img src="<?php echo get_bloginfo('template_directory');?>/img/SOJ-logo-wit.svg" alt="Shot of Joy">
 		<div class="content">
 			<h1>Je verlaat nu shotofjoy.nl</h1>
 			<p>We sturen je door naar <a href="<?php echo $shop_url;?>"><?php echo $shop_clean_url[2]; ?></a></p>
