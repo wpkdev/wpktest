9681ec1a04590bf8890a86e91e74e627037c9827
Niels Hendriks <niels@wayneparkerkent.com>
2014-05-09 11:32:51 +0200

    responsieve 1.0

--- incl_homepopup.php
+++ incl_homepopup.php
@@ -1,6 +1,7 @@
 <div class="welcomepopup-box">
-	<a href="#" class="btn btn-circle btn-close btn-close-welcome-popup">X</a>
-	<div class="welcomepopup center">	
+	
+	<div class="welcomepopup">
+		<a href="#" class="btn btn-circle btn-close btn-close-welcome-popup">X</a>	
 		<img src="<?php echo $template_directory; ?>/img/SOJ-logo.png" alt="Shot of Joy">
 		<h1>het allerbeste begin van de dag</h1>
 		<p>Dagelijks voor maar &euro; 2,99 per maand</p>
