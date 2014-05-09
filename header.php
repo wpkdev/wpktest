47bfda48697af3196ecef3587cea66d93a5c0c9c
Niels Hendriks <niels@wayneparkerkent.com>
2014-05-09 11:10:33 +0200

    taal

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <!doctype html>
 
 
-<html xmlns:fb="http://ogp.me/ns/fb#">
+<html lang="nl">
 <head>
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
