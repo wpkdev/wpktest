149c134defa4c50a770da456405c52c2382be298
Arjen Cleij <arjen@wayneparkerkent.com>
2014-05-12 13:30:18 +0200

    Welcome popup

--- incl_homepopup.php
+++ incl_homepopup.php
@@ -1,12 +1,13 @@
 <div class="welcomepopup-box">
 	<a href="#" class="btn btn-circle btn-close btn-close-welcome-popup">X</a>
 	<div class="welcomepopup center">	
-		<img src="<?php echo $template_directory; ?>/img/SOJ-logo.png" data-pin-no-hover="true" alt="Shot of Joy">
-
+		<div class="shotofjoy-logo">
+			<img src="<?php echo $template_directory; ?>/img/SOJ-logo.png" data-pin-no-hover="true" alt="Shot of Joy">
+		</div>
 	
 	<div class="welcomepopup">
-		<a href="#" class="btn btn-circle btn-close btn-close-welcome-popup">X</a>	
-		<img src="<?php echo $template_directory; ?>/img/SOJ-logo.png" alt="Shot of Joy">
+		<!--<a href="#" class="btn btn-circle btn-close btn-close-welcome-popup">X</a>	-->
+		<!--<img src="<?php echo $template_directory; ?>/img/SOJ-logo.png" alt="Shot of Joy">-->
 		<h1>het allerbeste begin van de dag</h1>
 		<p>Dagelijks voor maar &euro; 2,99 per maand</p>
 		<p><a href="<?php echo get_site_url(); ?>/login" class="btn btn-soj btn-small">Login</a> <a href="<?php echo get_site_url();?>/register" class="btn btn-soj btn-small">Abonneer</a></p>
