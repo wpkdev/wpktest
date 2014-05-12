eeb88b7102ba5d7ebaa7189ba92f1faa3091cc5b
Niels Hendriks <niels@wayneparkerkent.com>
2014-05-12 09:48:07 +0200

    google analytics

--- header.php
+++ header.php
@@ -20,13 +20,22 @@
 	 ?>
        
 	<title>Shot of Joy</title>
-	
+	<?php wp_head(); ?>
   	<link rel="stylesheet" href="<?php echo $template_directory; ?>/style.css?v=1.01199" type="text/css" /> 
 	
   	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>	
 	
 	<link href='http://fonts.googleapis.com/css?family=Pathway+Gothic+One' rel='stylesheet' type='text/css'>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-12257116-31', 'shotofjoy.nl');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body id="page-top">
 
