632bf890ff3db7b3057002ab5628e6a1d397b971
Niels Hendriks <niels@wayneparkerkent.com>
2014-05-13 14:06:15 +0200

    login pages

--- page_users.php
+++ page_users.php
@@ -22,7 +22,7 @@ while ( have_posts() ) : the_post();
 	?>
 			<a href="http://www.shotofjoy.nl"><img src="<?php echo $template_directory; ?>/img/close-btn.jpg" class="cls-btn" ></a>
 	<?php
-	echo '<img src="'.$template_directory.'/img/previewvideo.jpg" id="previewvideo"/>';
+	echo '<a href="#video-modal" class="modalLink"><img src="'.$template_directory.'/img/previewvideo.jpg" id="previewvideo"/></a>';
 	
 	
 	echo '<h2>'.get_the_title().'</h2>';
@@ -30,6 +30,9 @@ while ( have_posts() ) : the_post();
 	echo '</div>';
 	
 endwhile;
+
+$video_url = 'http://player.vimeo.com/video/31795904?title=0&byline=0&portrait=0&wmode=transparent&color=ffffff';
+
 
 get_footer(); ?>
 
