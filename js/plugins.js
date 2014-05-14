0d20891340d2b6204d901b74a67a53ea929aee53
Arjen Cleij <arjen@wayneparkerkent.com>
2014-05-14 21:54:38 +0200

    Title test

--- js/plugins.js
+++ js/plugins.js
@@ -66,4 +66,52 @@
 // Works with either jQuery or Zepto
 })( window.jQuery || window.Zepto );
 
+
+/* Fittext */
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
+
+
+/* Popeasy */
 (function(e){e.fn.modal=function(t){function o(){e("."+t.videoClass).attr("src",t.video);r.hide();i.css({top:e(window).height()/2-i.outerHeight()/2+e(window).scrollTop(),left:e(window).width()/2-i.outerWidth()/2+e(window).scrollLeft()});if(s===false){n.css({opacity:t.opacity,backgroundColor:"#"+t.background});n[t.animationEffect](t.animationSpeed);i.delay(t.animationSpeed)[t.animationEffect](t.animationSpeed)}else{i.show()}s=true}function u(){r.stop(true).animate({top:e(window).height()/2-r.outerHeight()/2+e(window).scrollTop(),left:e(window).width()/2-r.outerWidth()/2+e(window).scrollLeft()},t.moveModalSpeed)}function a(){e("."+t.videoClass).attr("src","");s=false;r.fadeOut(100,function(){if(t.animationEffect==="slideDown"){n.slideUp()}else if(t.animationEffect==="fadeIn"){n.fadeOut()}});return false}t=e.extend({trigger:".modalLink",olay:"div.overlay",modals:"div.modal",animationEffect:"fadeIn",animationSpeed:400,moveModalSpeed:"slow",background:"000",opacity:.8,openOnLoad:false,docClose:true,closeByEscape:true,moveOnScroll:true,resizeWindow:true,video:"",videoClass:"video",close:".closeBtn"},t);var n=e(t.olay);var r=e(t.modals);var i;var s=false;if(t.animationEffect==="fadein"){t.animationEffect="fadeIn"}if(t.animationEffect==="slidedown"){t.animationEffect="slideDown"}n.css({opacity:0});if(t.openOnLoad){o()}else{n.hide();r.hide()}e(t.trigger).bind("click",function(t){t.preventDefault();if(e(".modalLink").length>1){getModal=e(this).attr("href");i=e(getModal)}else{i=e(".modal")}o()});if(t.docClose){n.bind("click",a)}e(t.close).bind("click",a);if(t.closeByEscape){e(window).bind("keyup",function(e){if(e.which===27){a()}})}if(t.resizeWindow){e(window).bind("resize",u)}else{return false}if(t.moveOnScroll){e(window).bind("scroll",u)}else{return false}}})(jQuery)
